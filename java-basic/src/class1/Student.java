9bc9639e9bd657bda99d163419cbcf53bb78aca4
soojin <pofoxz@gmail.com>
2024-03-30 15:29:55 +0900

    Add: 학생 정보 출력 - Student 클래스

---
+++ java-basic/src/class1/Student.java
@@ -0,0 +1,15 @@
+package class1;
+
+// Student 클래스를 정의한다. = '학생'이라는 개념을 만든다.
+// 클래스는 대문자로 시작(관례)하며 낙타 표기법을 사용한다.
+
+public class Student {
+    // Student 클래스는 이름, 나이, 성적이라는 변수를 가진다.
+    // 클래스 내부에 정의한(소속된) 변수를 '멤버 변수' 혹은 '필드'라고 한다.
+    // 멤버 변수 (Member Variable) : 특정 클래스에 소속된 멤버
+    // 필드 (Field) : 각각의 데이터 항목을 가리키는 전통적인 용어
+
+    String name;
+    int age;
+    int grade;
+}
