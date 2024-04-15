a8def826ec506ef7def0417c9014f5f5f9cccc2f
soojin <pofoxz@gmail.com>
2024-04-15 23:10:18 +0900

    Refactor: 생성자 - 문제와 풀이 (생성자 코드 중복 제거)

--- java-basic/src/construct/ex/Book.java
+++ java-basic/src/construct/ex/Book.java
@@ -6,20 +6,27 @@ public class Book {
     int page;       // 페이지 수
 
     // 생성자 코드 작성
+    // 생성자 코드의 중복을 제거하기 위해 this()를 사용한다.
+    // => 모든 멤버 변수를 매개변수로 받는 생성자가 호출된다.
+
     // 1) 기본 생성자
     // 이미 생성자를 별도로 정의하였으므로, 기본 생성자를 사용하려면 직접 정의해주어야 한다.
     Book() {
         // 기본 생성자 내부를 비우지 않는다.
-        this.title = "";
-        this.author = "";
+//        this.title = "";
+//        this.author = "";
         // this.page = 0; 기본적으로 정수형 멤버 변수는 0으로 초기화된다.
+
+        this("", "", 0);
     }
 
     // 2) title, author만을 매개변수로 받는 생성자
     Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-        this.page = 0;
+//        this.title = title;
+//        this.author = author;
+//        this.page = 0;
+
+        this(title, author, 0);
     }
 
     // 3) 모든 멤버 변수를 매개변수로 받는 생성자
