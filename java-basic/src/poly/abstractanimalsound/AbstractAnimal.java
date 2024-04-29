d6c92d841d2a4af3b2a879b3b5addefe521bca3b
soojin <pofoxz@gmail.com>
2024-04-30 01:04:34 +0900

    Add: 추상 클래스 - Animal 클래스

--- java-basic/src/poly/abstractanimalsound/AbstractAnimal.java
+++ java-basic/src/poly/abstractanimalsound/AbstractAnimal.java
@@ -5,4 +5,9 @@ package poly.abstractanimalsound;
 public abstract class AbstractAnimal {
     // sound(): 메서드 바디 부분을 가지지 못하며, 자식 클래스에서 반드시 오버라이딩 해야 한다.
     public abstract void sound();
+
+    // 자식 클래스가 부모 클래스의 메서드를 사용할 목적으로 생성
+    public void move() {
+        System.out.println("동물이 움직입니다.");
+    }
 }
