64beabd1622242b6df3eb101a88d2301f80dd915
soojin <pofoxz@gmail.com>
2024-04-30 01:04:12 +0900

    Add: 추상 클래스 - Dog, Cat, Cow 클래스 (AbstractAnimal 상속)

---
+++ java-basic/src/poly/abstractanimalsound/Cat.java
@@ -0,0 +1,9 @@
+package poly.abstractanimalsound;
+
+public class Cat extends AbstractAnimal {
+    // 부모 클래스의 추상 메서드인 sound()를 반드시 오버라이딩 해야 한다.
+    @Override
+    public void sound() {
+        System.out.println("애옹!");
+    }
+}
